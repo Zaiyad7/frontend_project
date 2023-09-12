e6c16efc080f42d0c1aab1cd8dfefe18dbc4e723
Zaiyad7 <ZaiyadZakaria@live.co.uk>
2023-09-12 16:49:26 +0100

    rendered book image in book class

--- frontend_better_reads/src/components/Book.js
+++ frontend_better_reads/src/components/Book.js
@@ -2,6 +2,7 @@ const Book = ({ book }) => {
   return (
     <>
       <h4>{book.book.title}</h4>
+      <img src={require("../closedBook.png")} alt="book" />
       <ul>
         <li>Author : {book.book.author.name}</li>
         <li>Genre : {book.book.genre}</li>
