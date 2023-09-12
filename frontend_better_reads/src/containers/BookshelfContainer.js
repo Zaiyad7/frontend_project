6feb7bc66af68aff678e11a4afa5d5c3605c966f
hadams0 <adamshannah1@icloud.com>
2023-09-12 10:29:02 +0100

    use effect in bookshelf container

--- frontend_better_reads/src/containers/BookshelfContainer.js
+++ frontend_better_reads/src/containers/BookshelfContainer.js
@@ -1,7 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import User from "../components/User";
 
 const BookshelfContainer = () => {
   const [user, setUser] = useState(null);
+  const [users, setUsers] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/users")
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => {
+        console.log(response);
+      });
+  }, []);
+
+  return (
+    <>
+      <User />
+    </>
+  );
 };
 
 export default BookshelfContainer;
