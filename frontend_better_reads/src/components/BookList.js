6713907f64b56df114b26e32bac9ee70204a1e7f
Zaiyad7 <ZaiyadZakaria@live.co.uk>
2023-09-13 11:15:57 +0100

    added filter functionality in booklist

--- frontend_better_reads/src/components/BookList.js
+++ frontend_better_reads/src/components/BookList.js
@@ -1,9 +1,17 @@
 import Book from "./Book";
 
 const BookList = ({ bookshelves, selectedStatus }) => {
-  const bookComponents = bookshelves.map((book) => {
-    return <Book key={book.id} book={book} />;
+  const bookComponents = [];
+  bookshelves.forEach((book) => {
+    if (book.status === selectedStatus) {
+      bookComponents.push(<Book key={book.id} book={book} />);
+    }
   });
+  // const bookComponents = bookshelves.filter((book) => {
+  //   if (book.status === selectedStatus) {
+  //     return <Book key={book.id} book={book} />;
+  //   }
+  // });
 
   return (
     <>
