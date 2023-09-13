ca3167b23794066d2c2ce56b11f91c799a4e489d
hadams0 <adamshannah1@icloud.com>
2023-09-13 15:14:55 +0100

    tried to update status of book

--- frontend_better_reads/src/components/Book.js
+++ frontend_better_reads/src/components/Book.js
@@ -1,14 +1,46 @@
+import { useState, useEffect } from "react";
+
 const Book = ({ book }) => {
+  const [status, setStatus] = useState(book.status);
+  // console.log(book);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // console.log(event.target);
+    const form = event.target;
+    const formData = new FormData(form);
+    const formJson = Object.fromEntries(formData.entries());
+    // console.log(formJson.status);
+    setStatus(formJson.status);
+  };
+
+  useEffect(() => {
+    fetch(`http://localhost:8080/bookshelf/${book.id}/status`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ status: status }),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => {
+        console.log(response);
+      });
+  }, [status]);
+
   return (
     <>
       <h4>{book.book.title}</h4>
       <img src={require("../closedBook.png")} alt="book" />
       <ul>
         <li>Author : {book.book.author.name}</li>
-        <li>Genre : {book.book.genre}</li>
+        <li>
+          Genre :{" "}
+          {book.book.genre.charAt(0) + book.book.genre.toLowerCase().slice(1)}
+        </li>
         <li>Year : {book.book.year}</li>
       </ul>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="status">Status : </label>
         <select name="status" defaultValue={book.status}>
           <option value="unassign">Unassign</option>
